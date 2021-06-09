e859f118a7168195254079a7ebaae6cef70b27b4
Scott Cytacki <scytacki@concord.org>
2021-06-09 08:17:01 -0400

    script to generate release notes from PT stories

---
+++ scripts/release-notes.mjs
@@ -0,0 +1,59 @@
+import fetch from 'node-fetch';
+import querystring from 'querystring';
+
+const ptToken = process.env.PT_TOKEN;
+
+const features = [];
+const bugs = [];
+
+
+async function collectStories(projectId, search) {
+  const urlQuery = querystring.stringify(
+    {
+      query: search
+    }
+  );
+
+  // Documentation of this API is here: https://www.pivotaltracker.com/help/api/rest/v5#Search
+  const url = `https://www.pivotaltracker.com/services/v5/projects/${projectId}/search?${urlQuery}`
+
+  const response = await fetch(url, {
+    headers: { 'X-TrackerToken': ptToken }
+  });
+  const json = await response.json();
+  const stories = json.stories.stories
+  for(const story of stories) {
+    if(story.story_type === "feature"){
+      features.push(story);
+    }
+    if(story.story_type === "bug"){
+      bugs.push(story);
+    }
+  }
+}
+
+const ptLabel = process.argv[2];
+const search = `label:${ptLabel} includedone:true`;
+// Orange
+await collectStories(2441249, search)
+// Teal
+await collectStories(2441242, search)
+
+function storyItem(story) {
+  const name = story.name.replace(/\*\*\[[^\]]*\]\*\* ?/, "")
+  return `${name} [#${story.id}](${story.url})`
+}
+
+if (features.length > 0) {
+  console.log("### Features/Improvements");
+  for(const feature of features) {
+    console.log(`- ${storyItem(feature)}`);
+  }
+  console.log("");
+}
+if (bugs.length > 0) {
+  console.log("### Bug Fixes");
+  for(const bug of bugs) {
+    console.log(`- ${storyItem(bug)}`);
+  }
+}
